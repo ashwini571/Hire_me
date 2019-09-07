dbfd87adef9e2709bd5c0ae9cb4a5d4de088edbe
rht6226 <rohitraazanand567@gmail.com>
2019-09-07 12:11:39 +0530

    Base Templates

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from . import views
 
-urlpatterns=[
-    path('',home, name = 'home'),
+app_name = 'accounts'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
 ]
