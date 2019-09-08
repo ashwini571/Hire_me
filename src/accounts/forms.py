f9c225bfd2a0ce96326c93bf8c14283eab457f43
rht6226 <rohitraazanand567@gmail.com>
2019-09-08 13:54:44 +0530

    user profile formscreation and  GET done

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Client
+from .models import Client, UserProfile, Education, Project, Certifications
 
 
 class LoginForm(forms.Form):
@@ -8,6 +8,7 @@ class LoginForm(forms.Form):
 
 
 class ClientRegistrationForm(forms.ModelForm):
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput)
     password2 = forms.CharField(max_length=50, widget=forms.PasswordInput)
 
     class Meta:
@@ -20,10 +21,6 @@ class ClientRegistrationForm(forms.ModelForm):
         password = cd['password']
         password2 = cd['password2']
         email = cd['email']
-        first_name = cd['first_name']
-        last_name = cd['last_name']
-        type = cd['type']
-
         if password != password2:
             raise forms.ValidationError("Passwords did not match")
 
@@ -33,3 +30,46 @@ class ClientRegistrationForm(forms.ModelForm):
         if username and Client.objects.filter(username=username).exclude(email=email).count():
             raise forms.ValidationError("Username already exists")
 
+
+class UserProfileForm(forms.ModelForm):
+    gender = forms.ChoiceField(choices=UserProfile.SEXES)
+
+    class Meta:
+        model = UserProfile
+        fields = ['gender', 'about', 'skills', 'resume', 'languages']
+        widgets = {
+            'about': forms.Textarea(attrs={'rows': 4}),
+        }
+
+
+class EducationForm(forms.ModelForm):
+    class Meta:
+        model = Education
+        fields = ['school', 'degree', 'field_of_study', 'start_year', 'end_year', 'is_studying', 'description', 'grade']
+        widgets = {
+            'is_studying': forms.CheckboxInput(attrs={'class': 'col-xl-1'}),
+            'start_year': forms.DateInput(attrs={'class': 'col-xl-4 date-picker'}),
+            'end_year': forms.DateInput(attrs={'class': 'col-xl-4 date-picker'}),
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name', 'start_date', 'end_date', 'is_active', 'associated_with', 'project_URL', 'description']
+        widgets = {
+            'is_active': forms.CheckboxInput(attrs={'class': 'col-xl-1'}),
+            'start_date': forms.DateInput(attrs={'class': 'col-xl-4 date-picker'}),
+            'end_date' : forms.DateInput(attrs={'class': 'col-xl-4 date-picker'}),
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
+
+
+class CertificationsForm(forms.ModelForm):
+    class Meta:
+        model = Certifications
+        fields = ['name', 'issuing_organisation', 'issue_date', 'credential_ID', 'credential_URL']
+        widgets = {
+            'issue_date': forms.DateInput(attrs={'class': 'col-xl-4 date-picker'}),
+        }
--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views, form_views
 
 app_name = 'accounts'
 
@@ -8,4 +8,10 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('signup', views.registration_view, name='signup'),
+
+    # FormURLS
+    path('info/create_profile', form_views.create_user_profile_view, name='create_user_profile'),
+    path('info/add_education', form_views.add_education_view, name='add_education'),
+    path('info/add_project', form_views.add_project_view, name='add_education'),
+    path('info/add_certificate', form_views.add_project_view, name='add_education'),
 ]
